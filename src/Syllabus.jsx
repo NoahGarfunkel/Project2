30ab491ee95aea6608be9e6b0d45b245d30cec59
NoahGarfunkel <noahsgarfunkel@gmail.com>
2023-11-07 14:32:42 -0500

    Added Syllabus

---
+++ src/Syllabus.jsx
@@ -0,0 +1,71 @@
+import './Syllabus.css'
+
+function Syllabus() {
+  return (
+    <div className="Syllabus">
+      <h1> Syllabus </h1>
+      <body>
+    <h1>Course Title: User Interface Design</h1>
+    
+    <p><strong>Instructor:</strong> Noah Garfunkel</p>
+    <p><strong>Contact Information:</strong> garfunns@mail.uc.edu</p>
+    
+    <h2>Course Description:</h2>
+    <p>
+        This course is designed to provide students with a comprehensive understanding of User Interface (UI) design principles and practices. User Interface is a critical aspect of modern technology, as it directly impacts how users interact with digital products, websites, and applications. In this class, we will explore the fundamental concepts of UI design, usability, and user-centered design principles. Students will learn how to create effective and user-friendly interfaces through a combination of theoretical knowledge and practical hands-on exercises.
+    </p>
+    
+    <h2>Learning Objectives:</h2>
+    <ul>
+        <li>Understand the principles of user-centered design and usability.</li>
+        <li>Analyze and critique existing user interfaces.</li>
+        <li>Create wireframes and prototypes to design user-friendly interfaces.</li>
+        <li>Implement design principles for web and mobile applications.</li>
+        <li>Conduct usability testing to evaluate and improve UI designs.</li>
+        <li>Apply best practices for responsive design and accessibility.</li>
+    </ul>
+    
+    <h2>Course Schedule:</h2>
+    <h3>Week 1: Introduction to User Interface Design</h3>
+    <ul>
+        <li>What is UI Design?</li>
+        <li>The role of UI in user experience.</li>
+        <li>Historical perspective on UI design.</li>
+    </ul>
+    
+    <h2>Grading:</h2>
+    <ul>
+        <li>Class Participation: 10%</li>
+        <li>Homework Assignments: 30%</li>
+        <li>Midterm Project: 20%</li>
+        <li>Final Project: 30%</li>
+        <li>Quizzes and Exams: 10%</li>
+    </ul>
+    
+    <h2>Textbook:</h2>
+    <p>"Don't Make Me Think" by Steve Krug</p>
+    
+    <h2>Resources:</h2>
+    <ul>
+        <li>Online articles and tutorials</li>
+        <li>Design software tools (e.g., Figma, Sketch, Adobe XD)</li>
+    </ul>
+    
+    <h2>Prerequisites:</h2>
+    <p>There are no specific prerequisites for this course, but a basic understanding of design principles and software skills will be helpful.</p>
+    
+    <h2>Class Policies:</h2>
+    <ul>
+        <li>Attendance is mandatory.</li>
+        <li>Assignments and projects should be submitted on time.</li>
+        <li>Plagiarism and academic dishonesty will not be tolerated.</li>
+    </ul>
+    
+    <h2>Contact Information:</h2>
+    <p>If you have any questions or need assistance, please feel free to reach out to the instructor via email at garfunns@mail.uc.edu.</p>
+</body>
+    </div>
+  )
+}
+
+export default Syllabus
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Syllabus from './Syllabus'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Syllabus />
+  </React.StrictMode>,
+)
