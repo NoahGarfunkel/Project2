504f49e72169a15ef36c11cad06ec02606ab95a9
HouseAlex <houseah@mail.uc.edu>
2023-11-07 20:56:57 -0500

    routing

---
+++ src/components/shared/header/Header.jsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import Button from "@mui/material/Button";
+import "./Header.css";
+
+function Header() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          variant="h6"
+          component="a"
+          sx={{
+            flexGrow: 0,
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+          href="/"
+        >
+          Canvas
+        </Typography>
+        <Button color="inherit" href="/class/syllabus">
+          Syllabus
+        </Button>
+        <Button color="inherit" href="/class/announcments">
+          Announcements
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
