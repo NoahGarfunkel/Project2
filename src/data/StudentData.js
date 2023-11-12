dae2187dd1eaeb9bbe1e855e9fa518462bdcd509
HouseAlex <houseah@mail.uc.edu>
2023-11-12 14:00:35 -0500

    switch student

---
+++ src/data/StudentData.js
@@ -0,0 +1,16 @@
+export const GetStudents = () => {
+    return [
+        {
+            Id: 1,
+            Name: "Student 1",
+        },
+        {
+            Id: 2,
+            Name: "Student 2",
+        },
+        {
+            Id: 3,
+            Name: "Student 3"
+        }
+    ]
+}
